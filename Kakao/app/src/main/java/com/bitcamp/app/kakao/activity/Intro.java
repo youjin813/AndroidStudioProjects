2765bbe031cc83b03c6b5278738c7532684e21f2
youjin813 <youjin813@naver.com>
2018-02-06 14:28:35 +0900

    오후 2:28 2018-02-06

--- Kakao/app/src/main/java/com/bitcamp/app/kakao/activity/Intro.java
+++ Kakao/app/src/main/java/com/bitcamp/app/kakao/activity/Intro.java
@@ -1,7 +1,12 @@
 package com.bitcamp.app.kakao.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.bitcamp.app.kakao.R;
 
@@ -11,5 +16,16 @@ public class Intro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro);
+        final Context context = Intro.this;
+        final EditText phone = findViewById(R.id.phone);
+
+        findViewById(R.id.next_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"들어온 핸드폰 번호"+String.valueOf(phone.getText()),Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(context,Index.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ Kakao/app/src/main/java/com/bitcamp/app/kakao/activity/MemberDetail.java
@@ -0,0 +1,34 @@
+package com.bitcamp.app.kakao.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.bitcamp.app.kakao.R;
+
+/**
+ * Skeleton of an Android Things activity.
+ * <p>
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ * <p>
+ * <pre>{@code
+ * PeripheralManagerService service = new PeripheralManagerService();
+ * mLedGpio = service.openGpio("BCM6");
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+ * mLedGpio.setValue(true);
+ * }</pre>
+ * <p>
+ * For more complex peripherals, look for an existing user-space driver, or implement one if none
+ * is available.
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ */
+public class MemberDetail extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.member_detail);
+    }
+}
